93368a58d2756cb737f08d22197409610de57561
Hyung-Soo Kim <hyungsok@cisco.com>
2016-06-14 14:03:53 +0900

    update script.js & html

--- app/static/script.js
+++ app/static/script.js
@@ -2,33 +2,22 @@
 
 $(document).ready(function() {
   $("button").click(function() {
-/**
-    $.getJSON(url:"http://10.72.86.107:5500/test?callback=?", function(data) {
-       if( data != null) {
-       alert(data.result + ", " + data.go);
- 	}
-    });
-*/
     $.ajax({
       url: "http://10.72.86.107:5500/test",
       dataType: "jsonp",
       jsonpCallback: "myCallback",
       success: function(data,status,xhr) {
-        alert("call success " + status + "header="+xhr);
-        $("#update").html("whomai");
+        $("#update").html( data.name);
         if (data != null) {
-          alert(data.result + ", " + data.go);
-        } else {
-		  alert( "data is null");
-		}
+          alert(data.result + ", " + data.name);
+        } 
       },
-	 error: function(xhr) {
+	  error: function(xhr) {
         alert("call error");
         if (xhr!= null) {
           alert(xhr);
 		}
      }
     });
-
   });
 });
